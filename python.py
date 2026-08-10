992a0e429823e7dfb5b532e452e35682f42017f6
nolannziokicom <nolannzioki@gmail.com>
2026-08-10 20:35:00 +0300

    This is my first python file

---
+++ python.py
@@ -0,0 +1,14 @@
+# My 1st python file
+print("Hello world of coding")
+
+#print my name
+print("I'm Nolan")
+
+#my age
+print("I'm 12 turning 13")
+
+#my country
+print("i live in Kenya and im proud of my country")
+
+
+print("im currently learning about coding specifically python")
