a4a9a1d9b9fb35407d231e4ce91a4a8bda7b7985
nolannziokicom <nolannzioki@gmail.com>
2026-08-12 19:29:14 +0300

    this is my daily routine

---
+++ My daily routine ass.py
@@ -0,0 +1,29 @@
+# how i start my day
+
+print("I wake up and pray right after i go wash my face")
+
+#activties i do to prepare for school
+#my 1st activity
+print("i go and take a bath right after")
+
+#my 2nd activity
+print("i then wear my uniform")
+
+#my 3rd activity
+print("i go and take breakfast")
+
+print("then i brush my teeth and head to school")
+
+#actvities i do after school
+
+print("when i reach home i wash my hands")
+
+print("i then polish my school shoes")
+
+print("i wait for supper right after ")
+
+print("After taking supper i prepare my school uniform")
+
+print("i take water then go to sleep")
+
+print("That's my daily routine")
