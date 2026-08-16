1d8ef206c951f01036fa620ab7f20560eecc2def
nolannziokicom <nolannzioki@gmail.com>
2026-08-16 20:05:43 +0300

    this is showing some info abt my friends

---
+++ storing bithdays.py
@@ -0,0 +1,26 @@
+
+#My 2 friends name goal and about themselves
+
+friendsname=("Lincoln")
+school=("Machakos school")
+birthday=(8,"of October")
+hobby=("Likes playing games")
+
+print ("this is my friend lincoln")
+print(friendsname)
+print(school)
+print(birthday)
+print(hobby)
+
+#my 2nd friends goals name etc
+
+friend2sname=("Daniel")
+friend2school=("Machakos School")
+friend2birthday=("september 3rd")
+danielshobby=("Enjoys playing with his dogs")
+
+print("this is my other friend Daniel")
+print(friend2sname)
+print(friend2school)
+print(friend2birthday)
+print(danielshobby)
