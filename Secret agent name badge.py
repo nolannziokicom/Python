ca905c1d3cf047aa2c8d3ea2c075025136158b10
nolannziokicom <nolannzioki@gmail.com>
2026-08-19 20:33:17 +0300

    This is showing my secret agent name badge in python

--- Secret agent name badge.py
+++ Secret agent name badge.py
@@ -20,33 +20,23 @@ print(type(active_status))
 print(type(height))
 
 
-""""
-3) Display each value and its data type.
-   a) Print the agent name and gadget.
-   b) Print number, rating, mission count, height, and active status.
-   c) Use `type()` to show the data type of each value.
+Agent_number_text=str(Agent_number)
+Speed_text=str(Speed)
+mission_count_text=str(mission_count)
+active_status_text=str(active_status)
+height_text=str(height)
 
-4) Convert values into text.
+print(type(Agent_number_text))
+print(type(Speed_text))
+print(type(mission_count_text))
+print(type(active_status_text))
+print(type(height_text))
+
+
+
+''''
+) Convert values into text.
    a) Use `str()` to convert numbers into strings.
    b) Convert the Boolean value into text.
    c) Print the converted values and their new data types.
-
-5) Create a secret code name.
-   a) Use slicing to get the first three letters of the name.
-   b) Use negative indexing to get the last letter.
-   c) Join both parts to create the code name.
-
-6) Reverse the gadget name.
-   a) Use slicing with `[::-1]` to reverse the gadget text.
-   b) Print the reversed gadget name.
-
-7) Build the badge message.
-   a) Create separate lines for the agent badge.
-   b) Use string concatenation to join text and variables.
-   c) Use `.upper()` to make important badge text uppercase.
-
-8) Print the secret agent badge.
-   a) Print a badge heading.
-   b) Print all badge lines one by one.
-   c) Print a closing line to complete the badge.
-"""
+'''
