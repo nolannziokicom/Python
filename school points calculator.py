604f1f4947b8c51919f3eb66441f6ed60efc5dfc
nolannziokicom <nolannzioki@gmail.com>
2026-09-01 19:36:22 +0300

    this is school points calculator it tells you your points for an exam

---
+++ school points calculator.py
@@ -0,0 +1,17 @@
+points= input("Please enter your points:")
+
+if points < "40":
+    print("Your are in the Below expectation points")
+    print("Please revise and pull up your socks")
+elif points < "65":
+    print("You are in approaching expectation")
+    print("Not bad but you could do better")
+elif points < "76":
+    print("You are in meeting expectation")
+    print("You have done a good job")
+elif points < "100":
+    print("You are in exceeding expectaion")
+    print("Great job!")
+
+print()
+print("Good luck and start revising for the next exam")
