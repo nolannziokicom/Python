13fa2b78c44ba2c565f944fe4b6993627c17fbe5
nolannziokicom <nolannzioki@gmail.com>
2026-09-15 20:35:01 +0300

    this is my app that makes sure you have completed your work

---
+++ Homework completion calculator.py
@@ -0,0 +1,12 @@
+print("Homework completion calcutor")
+ask=input("Have you finished your homework?(yes/no)")
+if ask=="no":
+    print("Lets get started")
+    ask1=input("Have you finished your maths homework")
+    if ask1 == "no":
+        print("Please finish your maths home work")
+        print("Are you done")
+    else:
+        print("Then finish all your other assingments and do your chores after")
+  
+
