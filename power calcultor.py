bd638d642cbed49e9eb8b7159c074cdf479a7327
nolannziokicom <nolannzioki@gmail.com>
2026-09-15 20:25:33 +0300

    this is my power calcultor in python

--- power calcultor.py
+++ power calcultor.py
@@ -12,13 +12,3 @@ for i in range(1,power+1):
 
 
 
-# ---------- PART 3 + 4: the loop that multiplies ----------
-# YOUR CODE HERE
-# for i in range(1, exponent + 1):
-#     multiply result by base and store it back in result
-#     print which step you are on and what result is now
-
-
-# ---------- PART 5: the answer ----------
-# YOUR CODE HERE
-# After the loop, print the base, the exp
