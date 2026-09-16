f881edd17d9603716ab1ba4a17296881f16a14fd
nolannziokicom <nolannzioki@gmail.com>
2026-09-16 20:44:27 +0300

    this is showing my pattern of symbols in right angled triangles in python

---
+++ Right angled triangle.py
@@ -0,0 +1,7 @@
+print("We are making a right angled triangle pattern")
+n=int(input("Enter the number of rows:"))
+
+for i in range(n):
+    for j in range(i+1):
+        print("$",end="")
+    print()
--- School club membership badge.py
+++ School club membership badge.py
@@ -4,12 +4,33 @@ name=input("Enter your name:")
 classroom=input("Enter your class:")
 Reasonfortryingtojoin=input("Please right why you want to join our club:")
 Reasonforlettingyouintoourclub=input("Please let us know why we should let you into our club:")
-print(name)
-print(classroom)
-print(Reasonfortryingtojoin)
-print(Reasonforlettingyouintoourclub)
+membernumber="0"
+meetinghours=1215
+upcomingevents=3
+activestatus="New member"
 
-print("NOW OFFICIALLY PART OF 4K CLUB")
+print(type(activestatus))
+print(type(meetinghours))
+print(type(upcomingevents))
+print(type(activestatus))
+
+meetinghours=str(meetinghours)
+membernumber=int(membernumber)
+
+upcomingevents=str(upcomingevents)
+print(type(meetinghours))
+print(type(membernumber))
+
+print(type(upcomingevents))
+
+
+
+
+
+
+
+
+
 
 
 
